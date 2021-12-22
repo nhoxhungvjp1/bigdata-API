b95ac3efbcb4bff51578eaf31b7c14ae2633820f
nhoxhungvjp1 <74084773+nhoxhungvjp1@users.noreply.github.com>
2021-12-22 21:01:44 +0700

    Add files via upload

---
+++ models/Todo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+
+var Todo = new mongoose.Schema({
+    task: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 30,
+    },
+}, {timestamps: true});
+
+
+module.exports = mongoose.model("Todo", Todo);
